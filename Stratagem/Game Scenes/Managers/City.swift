0596b5e16bf9a3f90ef7f32f0f64d87fb9f86360
Andrew Peng <andrewpeng02@gmaio.com>
2020-11-24 15:21:19 -0600

    staggered iso maps??

--- Stratagem/Game Scenes/Managers/City.swift
+++ Stratagem/Game Scenes/Managers/City.swift
@@ -1,5 +1,4 @@
 import SpriteKit
-import GameplayKit
 import SKTiled
 
 public class City {
@@ -10,7 +9,7 @@ public class City {
     var cityTerrain: [[Int]]?
     
     /// City size
-    let citySize: CGSize = CGSize(width: 32, height: 32)
+    let citySize: CGSize = CGSize(width: 12, height: 24)
     
     /// Initializes city variables (required)
     func initCity(cityName: String) {
@@ -44,8 +43,8 @@ public class City {
         }
         layer2 = String(layer2.dropLast(3)) + "\n"
         
-        var text = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<map version=\"1.4\" tiledversion=\"1.4.3\" orientation=\"isometric\" renderorder=\"right-down\" width=\"" + String(Int(citySize.width)) + "\" height=\""
-        text = text + String(Int(citySize.height)) + "\" tilewidth=\"256\" tileheight=\"128\" infinite=\"0\" nextlayerid=\"11\" nextobjectid=\"1\">\n <tileset firstgid=\"1\" source=\"PrototypePack.tsx\"/>\n <layer id=\"8\" name=\"Tile Layer 1\" width=\"" + String(Int(citySize.width)) + "\" height=\""
+        var text = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<map version=\"1.4\" tiledversion=\"1.4.3\" orientation=\"staggered\" renderorder=\"right-down\" width=\"" + String(Int(citySize.width)) + "\" height=\""
+        text = text + String(Int(citySize.height)) + "\" tilewidth=\"256\" tileheight=\"128\" infinite=\"0\" staggeraxis=\"y\" staggerindex=\"odd\" nextlayerid=\"11\" nextobjectid=\"1\">\n <tileset firstgid=\"1\" source=\"PrototypePack.tsx\"/>\n <layer id=\"8\" name=\"Tile Layer 1\" width=\"" + String(Int(citySize.width)) + "\" height=\""
         text = text + String(Int(citySize.height)) + "\">\n  <data encoding=\"csv\">\n" + layer1 + "</data>\n </layer>\n <layer id=\"9\" name=\"Tile Layer 2\" width=\"" + String(Int(citySize.width)) + "\" height=\"" + String(Int(citySize.height)) + "\">\n  <data encoding=\"csv\">\n" + layer2 + "</data>\n </layer>\n</map>\n"
         
         if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
@@ -65,12 +64,12 @@ public class City {
         let num_columns = Int(citySize.height)
         let num_rows = Int(citySize.width)
         //let noiseMap = makeNoiseMap(columns: num_columns, rows: num_rows, persistence: 0.9)
-        let noisemap = Perlin2D().octaveMatrix(width: 128, height: 128, octaves: 6, persistance: 0.25)
+        let noisemap = Perlin2D().octaveMatrix(width: num_rows * 4, height: num_columns * 4, octaves: 6, persistance: 0.25)
         
         var terrain: [[Int]] = Array(repeating: Array(repeating: 0, count: Int(citySize.height)), count: Int(citySize.width))
         // Downsizes a 128x128 matrix by averaging every 4x4 sub-matrix
-        for row in 0..<32 {
-            for col in 0..<32 {
+        for row in 0..<num_rows {
+            for col in 0..<num_columns {
                 let x_st = 4 * row
                 let y_st = 4 * col
                 
@@ -84,9 +83,9 @@ public class City {
                     }
                 }
                 let avgTerrainHeight = total / 16
-                if avgTerrainHeight <= 0.47 {
+                if avgTerrainHeight <= 0.37 {
                     terrain[row][col] = 2
-                } else if avgTerrainHeight <= 0.53 {
+                } else if avgTerrainHeight <= 0.43 {
                     terrain[row][col] = 3
                 } else if avgTerrainHeight <= 1 {
                     terrain[row][col] = 1
@@ -95,19 +94,6 @@ public class City {
             cityTerrain = terrain
         }
     }
-    
-    /// Creates a perlin noise map
-//    private func makeNoiseMap(columns: Int, rows: Int, persistence: Double) -> GKNoiseMap {
-//        let source = GKPerlinNoiseSource()
-//        source.persistence = persistence
-//
-//        let noise = GKNoise(source)
-//        let size = vector2(1.0, 1.0)
-//        let origin = vector2(0.0, 0.0)
-//        let sampleCount = vector2(Int32(columns), Int32(rows))
-//
-//        return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
-//    }
     
     private func copyFileToDocumentsFolder(nameForFile: String, extForFile: String) {
         
