2a88ff22a669e1048fda173597c4364100458878
Andrew Peng <andrewpeng02@gmaio.com>
2020-11-29 17:58:52 -0600

    Refactored gamevariables into more classes

--- Stratagem/Variables/GameVariables.swift
+++ Stratagem/Variables/GameVariables.swift
@@ -1,6 +1,5 @@
 // Stores variables for high-frequency game updates
 
-import Combine
 import SceneKit
 import SpriteKit
 
@@ -17,51 +16,14 @@ class GameVariables {
     var gameType: GameTypes = GameTypes.STANDARD
     
     // Directly determines display
-    var selectedPlanet: PlanetView = PlanetView(planetID: 0)
+    var selectedPlanet: Int = 0
     var selectedCity: City?
     
-    var galaxy: GalaxyView
-    var galaxyLayout: [PlanetLayout] = []
-    
-    var galaxyScene: SKScene?
-    
+    var galaxy: Galaxy
+        
     func generateGalaxy(){
-        var numPlanets: Int
-        switch gameType {
-        case .STANDARD:
-            numPlanets = 7
-        default:
-            numPlanets = 5
-        }
-        for i in 0..<numPlanets {
-            galaxyLayout.append(PlanetLayout(planetID: i, planet: PlanetView(planetID: i)))
-            galaxyLayout[i].planetNode = galaxyLayout[i].planet.planetNode
-        }
+        galaxy.generateNewGalaxy(gameType)
     }
-    init(){galaxy = GalaxyView()} /// generates our first galaxy. generateGalaxy can be executed again to create new galaxy
-}
-
-struct PlanetLayout {
-    var planetID: Int!
-    var planet: PlanetView!
-    var planetNode: SCNNode?
-    
-    // Later when city count/position is random these will need to be procedurally generated
-    var cities: [City] = []
-    let cityMapping = [
-        CGRect(x: 353, y: 153, width: 167, height: 90),
-        CGRect(x: 353, y: 183, width: 144, height: 99),
-        CGRect(x: 392, y: 148, width: 142, height: 82)
-    ]
-    
-    init(planetID: Int, planet: PlanetView) {
-        self.planet = planet
-        self.planetID = planetID
-        for i in 0..<cityMapping.count {
-            let city = City()
-            city.initCity(cityName: "City Name")
-            cities.append(city)
-        }
-    }
+    init(){galaxy = Galaxy()} /// generates our first galaxy. generateGalaxy can be executed again to create new galaxy
 }
 
