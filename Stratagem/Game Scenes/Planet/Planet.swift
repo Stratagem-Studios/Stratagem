2d20fa911c26c540e144b80c19ff2158292b9a67
Andrew Peng <andrewpeng02@gmaio.com>
2020-11-30 12:38:28 -0600

    edit var

--- Stratagem/Game Scenes/Planet/Planet.swift
+++ Stratagem/Game Scenes/Planet/Planet.swift
@@ -7,7 +7,7 @@ class Planet {
     // Later when city count/position is random these will need to be procedurally generated
     var cities: [City] = []
 
-    let cityMapping = [
+    var cityMapping = [
         CGRect(x: 0.705, y: 0.431, width: 0.145, height: 0.139)
         //CGRect(x: 353, y: 183, width: 144, height: 99),
         //CGRect(x: 392, y: 148, width: 142, height: 82)
