6149c5a7187b064a207e2febd9bad38c025e81c7
LifeDrain28 <nirajmamin@outlook.com>
2020-12-08 17:32:45 -0600

    light code refactor

--- Stratagem/Game Scenes/City/CityScene.swift
+++ Stratagem/Game Scenes/City/CityScene.swift
@@ -4,9 +4,9 @@ import SwiftUI
 
 
 public class CityScene: SKTiledScene {
-    private var city = City()
+    private var city: City?
     private var cityEditState = CityEditStates.NONE
-    var playerVariables: PlayerVariables?
+    var playerVariables = Global.playerVariables 
     
     private let hudNode = HudNode()
     
@@ -15,39 +15,32 @@ public class CityScene: SKTiledScene {
     
     public override func didMove(to view: SKView) {
         city = Global.gameVars!.selectedCity!
-        city.createTMXFile()
-        
+        city!.createTMXFile()
+
         super.didMove(to: view)
         super.setup(tmxFile: "City")
         cameraNode.allowGestures = true
         cameraNode.setCameraZoom(0.4)
         cameraNode.setZoomConstraints(minimum: 0.3, maximum: 0.75)
         cameraNode.showOverlay = true
-        
-        city.loadTilemap(tilemap)
-        
+
+        city!.loadTilemap(tilemap)
+        print(city?.cityTerrain.count)
+
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(sceneTapped(_:)))
         self.view!.addGestureRecognizer(tapGestureRecognizer)
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(scenePan(_:)))
         self.view!.addGestureRecognizer(panGestureRecognizer)
-        
+
         view.showsFPS = true
         view.showsDrawCount = true
-        
-        hudNode.setup(city: city, size: size, view: view, tilemap: tilemap)
+
+        hudNode.setup(city: city!, size: size, view: view, tilemap: tilemap)
         cameraNode.addToOverlay(hudNode)
-        city.hudNode = hudNode
-        
+        city!.hudNode = hudNode
+
         changeStateToNone()
         
-        // Updates HUD every 1 sec
-        let wait = SKAction.wait(forDuration: 0.5)
-        let update = SKAction.run({ [self] in
-            hudNode.update()
-        })
-        let seq = SKAction.sequence([update, wait])
-        let repeatActions = SKAction.repeatForever(seq)
-        run(repeatActions)
     }
     
     public override func willMove(from view: SKView) {
@@ -147,10 +140,10 @@ public class CityScene: SKTiledScene {
                         }
                     case .BUILD:
                         if let buildSelectedTiledata = buildSelectedTiledata {
-                            city.changeTileAtLoc(firstTile: tile, secondTileID: buildSelectedTiledata.globalID)
+                            city!.changeTileAtLoc(firstTile: tile, secondTileID: buildSelectedTiledata.globalID)
                         }
                     case .DESTROY:
-                        city.changeTileAtLoc(firstTile: tile, secondTileID: 1)
+                        city!.changeTileAtLoc(firstTile: tile, secondTileID: 1)
                     }
                 }
             }
@@ -185,7 +178,7 @@ public class CityScene: SKTiledScene {
     }
     
     private func highlightUneditableTiles(colorBlendFactor: CGFloat) {
-        for cityTile in city.cityTerrain!.joined() {
+        for cityTile in city!.cityTerrain!.joined() {
             if !cityTile.isEditable! {
                 let tile = cityTile.tile
                 let action = SKAction.colorize(with: .black, colorBlendFactor: colorBlendFactor, duration: 1)
